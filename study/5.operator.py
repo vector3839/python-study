1e74d9206ce151070418daad22338c1ca2e75faf
vector <vector3839@gmail.com>
2026-03-08 00:16:02 +0800

    main

---
+++ study/5.operator.py
@@ -0,0 +1,14 @@
+a = 5
+b = 2
+# +-*/
+print(1 / 1) #除法，结果是浮点数
+print(a // b) #整除，结果是整数，且向下取整
+print(a % b) #取模，结果是a除以b的余数
+print(a ** b) #幂运算，结果是a的b次幂
+
+a += 1 # a = a + 1
+print(a)
+a -= 1 # a = a - 1
+print(a)
+a += b # a = a + b
+print(a)

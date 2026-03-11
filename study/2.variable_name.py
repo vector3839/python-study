9b8c6ef93069f0de666b24b955f768e520bfccc2
vector <vector3839@gmail.com>
2026-03-11 16:25:40 +0800

    main

--- study/2.variable_name.py
+++ study/2.variable_name.py
@@ -6,13 +6,13 @@ num3 = 6.66
 print(num3)
 num3 = "hello world"
 print(num3)
-秦一杰 = "sb"
-秦二杰 = "sb"
-print(秦一杰)
-print(秦二杰)
+steve1 = "sb"
+steve2 = "sb"
+print(steve1)
+print(steve2)
 
 # 变量命名规则：只能包含字母、数字和下划线，不能以数字开头，不能使用Python的关键字。
 # 变量名区分大小写，变量名应该具有描述性，能够清晰地表达变量的含义。
-user_name = "秦一杰"
-userName = "秦二杰"
-UserName = "秦三杰"
+user_name = "steve1"
+userName = "steve2"
+UserName = "steve3"

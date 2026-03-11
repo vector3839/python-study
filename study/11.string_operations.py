19ff1031a6c7df5243ac146947097bbc3dc4a928
vector <vector3839@gmail.com>
2026-03-11 13:39:19 +0800

    main

---
+++ study/11.string_operations.py
@@ -0,0 +1,11 @@
+
+print("he" in str1)
+print("elo" not in str1)
+
+print(str1[0])
+print(str1[-1])
+
+print(str1[:3]) # 从0开始，包含0，不包含3
+print(str1[1:4]) # 从1开始，包含1，不包含4
+print(str1[-3:-1]) # 从-3开始，包含-3，不包含-1
+print(str1[-1::-1])
